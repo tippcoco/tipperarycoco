f43fb86dec15d3d941e9fa4be157e5090dbea95e
tippcoco <donal.mcweeney@tipperarycoco.ie>
2014-06-20 14:29:24 +0100

    front page news feature
    test upload of feature created

--- sites/all/drush/aliases.drushrc.php
+++ sites/all/drush/aliases.drushrc.php
@@ -1,7 +1,17 @@
 <?php
-
-$aliases['dev'] = array(
+$aliases['devel'] = array(
  'root' => '/srv/drupal',
  'remote-host' => 'ssh.ac',
  'remote-user' => 'site-192a616e5a2f4adf99f8f3737bae2a36',
+);
+$aliases['staging'] = array(
+ 'root' => '/srv/drupal',
+ 'remote-host' => 'ssh.ac',
+ 'remote-user' => 'site-d1452412b1f347cbaaa71ff464556c69',
+);
+
+$aliases['live'] = array(
+ 'root' => '/srv/drupal',
+ 'remote-host' => 'ssh.ac',
+ 'remote-user' => 'site-215f167d8c004724bacb780f1cb5553e',
 );
