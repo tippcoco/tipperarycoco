bfe93bae62843a919fb565bbac01340b99b44a3e
tippcoco <donal.mcweeney@tipperarycoco.ie>
2014-06-20 15:48:14 +0100

    Revert "Revert "change features location""
    This reverts commit 4c3a65b9a8ed043284b38d1bb47c30f2c7dadac8.

--- sites/all/drush/aliases.drushrc.php
+++ sites/all/drush/aliases.drushrc.php
@@ -1,17 +1,7 @@
 <?php
-$aliases['devel'] = array(
+
+$aliases['dev'] = array(
  'root' => '/srv/drupal',
  'remote-host' => 'ssh.ac',
  'remote-user' => 'site-192a616e5a2f4adf99f8f3737bae2a36',
-);
-$aliases['staging'] = array(
- 'root' => '/srv/drupal',
- 'remote-host' => 'ssh.ac',
- 'remote-user' => 'site-d1452412b1f347cbaaa71ff464556c69',
-);
-
-$aliases['live'] = array(
- 'root' => '/srv/drupal',
- 'remote-host' => 'ssh.ac',
- 'remote-user' => 'site-215f167d8c004724bacb780f1cb5553e',
 );
